7c6d1f74d9bf2685a03b2d7fe6b1f4c9c0bb1f69
Aizad-eng <dxuxama@gmail.com>
2024-10-31 22:49:05 +0500

    Add 15-second delay to the response

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+import time  # Import the time module
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello world', 200
+    time.sleep(15)  # Delay the response by 15 seconds
+    return jsonify(message='Hello world'), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
